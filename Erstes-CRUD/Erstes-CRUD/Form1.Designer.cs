aca6eebf7c0d37ae51ab357e52d317bc7db62952
Kristian Lubina <kristian.lubina@outlook.com>
2024-11-10 14:09:31 +0100

    erstes funktionierendes CRUD

---
+++ Erstes-CRUD/Erstes-CRUD/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace EinfachesCRUD
+  {
+  partial class Form1
+    {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose( bool disposing )
+      {
+      if ( disposing && ( components != null ) )
+        {
+        components.Dispose();
+        }
+      base.Dispose( disposing );
+      }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+      {
+      this.txtName = new System.Windows.Forms.TextBox();
+      this.btnAdd = new System.Windows.Forms.Button();
+      this.btnUpdate = new System.Windows.Forms.Button();
+      this.btnDelete = new System.Windows.Forms.Button();
+      this.btnRead = new System.Windows.Forms.Button();
+      this.dataGridViewStudents = new System.Windows.Forms.DataGridView();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // txtName
+      // 
+      this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+      this.txtName.Location = new System.Drawing.Point(48, 40);
+      this.txtName.Name = "txtName";
+      this.txtName.Size = new System.Drawing.Size(100, 34);
+      this.txtName.TabIndex = 0;
+      // 
+      // btnAdd
+      // 
+      this.btnAdd.Location = new System.Drawing.Point(181, 30);
+      this.btnAdd.Name = "btnAdd";
+      this.btnAdd.Size = new System.Drawing.Size(138, 44);
+      this.btnAdd.TabIndex = 1;
+      this.btnAdd.Text = "Hinzufügen";
+      this.btnAdd.UseVisualStyleBackColor = true;
+      this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+      // 
+      // btnUpdate
+      // 
+      this.btnUpdate.Location = new System.Drawing.Point(334, 30);
+      this.btnUpdate.Name = "btnUpdate";
+      this.btnUpdate.Size = new System.Drawing.Size(138, 44);
+      this.btnUpdate.TabIndex = 2;
+      this.btnUpdate.Text = "Aktualisieren";
+      this.btnUpdate.UseVisualStyleBackColor = true;
+      this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+      // 
+      // btnDelete
+      // 
+      this.btnDelete.Location = new System.Drawing.Point(478, 30);
+      this.btnDelete.Name = "btnDelete";
+      this.btnDelete.Size = new System.Drawing.Size(130, 44);
+      this.btnDelete.TabIndex = 3;
+      this.btnDelete.Text = "Löschen";
+      this.btnDelete.UseVisualStyleBackColor = true;
+      this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+      // 
+      // btnRead
+      // 
+      this.btnRead.Location = new System.Drawing.Point(614, 30);
+      this.btnRead.Name = "btnRead";
+      this.btnRead.Size = new System.Drawing.Size(127, 44);
+      this.btnRead.TabIndex = 4;
+      this.btnRead.Text = "Lesen";
+      this.btnRead.UseVisualStyleBackColor = true;
+      this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+      // 
+      // dataGridViewStudents
+      // 
+      this.dataGridViewStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridViewStudents.Location = new System.Drawing.Point(48, 98);
+      this.dataGridViewStudents.Name = "dataGridViewStudents";
+      this.dataGridViewStudents.RowHeadersWidth = 72;
+      this.dataGridViewStudents.RowTemplate.Height = 31;
+      this.dataGridViewStudents.Size = new System.Drawing.Size(693, 311);
+      this.dataGridViewStudents.TabIndex = 5;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.dataGridViewStudents);
+      this.Controls.Add(this.btnRead);
+      this.Controls.Add(this.btnDelete);
+      this.Controls.Add(this.btnUpdate);
+      this.Controls.Add(this.btnAdd);
+      this.Controls.Add(this.txtName);
+      this.Name = "Form1";
+      this.Text = "Erstes-CRUD";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+      }
+
+    #endregion
+    private System.Windows.Forms.TextBox txtName;
+    private System.Windows.Forms.Button btnAdd;
+    private System.Windows.Forms.Button btnUpdate;
+    private System.Windows.Forms.Button btnDelete;
+    private System.Windows.Forms.Button btnRead;
+    private System.Windows.Forms.DataGridView dataGridViewStudents;
+    }
+  }
+
---
+++ Erstes-CRUD/Erstes-CRUD/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace EinfachesCRUD
+  {
+  public partial class Form1: Form
+    {
+    // Verbindung zur Datenbank herstellen (angepasster Verbindungsstring)
+    private string connectionString = @"Server=DESKTOP-O9BMBCB\SQLEXPRESS;Database=Erstes-CRUD;Integrated Security=True;";
+
+    public Form1()
+      {
+      InitializeComponent();
+      }
+
+    private void TestDatabaseConnection()
+      {
+      using ( SqlConnection con = new SqlConnection( connectionString ) )
+        {
+        try
+          {
+          con.Open();
+          MessageBox.Show( "Verbindung zur Datenbank erfolgreich!" );
+          }
+        catch ( Exception ex )
+          {
+          MessageBox.Show( "Fehler bei der Verbindung zur Datenbank: " + ex.Message );
+          }
+        }
+      }
+
+    // Diese Methode kannst du dann zum Beispiel im Load-Event der Form aufrufen
+    private void Form1_Load( object sender, EventArgs e )
+      {
+      TestDatabaseConnection();
+      }
+
+    // CREATE Operation (Datensatz hinzufügen)
+    private void btnAdd_Click( object sender, EventArgs e )
+      {
+      using ( SqlConnection con = new SqlConnection( connectionString ) )
+        {
+        con.Open();
+        string query = "INSERT INTO Students (Name) VALUES (@Name)";
+        using ( SqlCommand cmd = new SqlCommand( query, con ) )
+          {
+          cmd.Parameters.AddWithValue( "@Name", txtName.Text );
+          cmd.ExecuteNonQuery();
+          MessageBox.Show( "Student hinzugefügt" );
+          LoadData();
+          }
+        }
+      }
+
+    // READ Operation (Daten anzeigen)
+    private void btnRead_Click( object sender, EventArgs e )
+      {
+      LoadData();
+      }
+
+    // Hilfsfunktion zum Laden der Daten in das DataGridView
+    private void LoadData()
+      {
+      using ( SqlConnection con = new SqlConnection( connectionString ) )
+        {
+        con.Open();
+        string query = "SELECT * FROM Students";
+        using ( SqlDataAdapter da = new SqlDataAdapter( query, con ) )
+          {
+          DataTable dt = new DataTable();
+          da.Fill( dt );
+          dataGridViewStudents.DataSource = dt;
+          }
+        }
+      }
+
+    // UPDATE Operation (Datensatz aktualisieren)
+    private void btnUpdate_Click( object sender, EventArgs e )
+      {
+      if ( dataGridViewStudents.SelectedRows.Count > 0 )
+        {
+        using ( SqlConnection con = new SqlConnection( connectionString ) )
+          {
+          con.Open();
+          string query = "UPDATE Students SET Name = @Name WHERE ID = @ID";
+          using ( SqlCommand cmd = new SqlCommand( query, con ) )
+            {
+            int id = Convert.ToInt32( dataGridViewStudents.SelectedRows[ 0 ].Cells[ 0 ].Value );
+            cmd.Parameters.AddWithValue( "@ID", id );
+            cmd.Parameters.AddWithValue( "@Name", txtName.Text );
+            cmd.ExecuteNonQuery();
+            MessageBox.Show( "Student aktualisiert" );
+            LoadData();
+            }
+          }
+        }
+      }
+
+    // DELETE Operation (Datensatz löschen)
+    private void btnDelete_Click( object sender, EventArgs e )
+      {
+      if ( dataGridViewStudents.SelectedRows.Count > 0 )
+        {
+        using ( SqlConnection con = new SqlConnection( connectionString ) )
+          {
+          con.Open();
+          string query = "DELETE FROM Students WHERE ID = @ID";
+          using ( SqlCommand cmd = new SqlCommand( query, con ) )
+            {
+            int id = Convert.ToInt32( dataGridViewStudents.SelectedRows[ 0 ].Cells[ 0 ].Value );
+            cmd.Parameters.AddWithValue( "@ID", id );
+            cmd.ExecuteNonQuery();
+            MessageBox.Show( "Student gelöscht" );
+            LoadData();
+            }
+          }
+        }
+      }
+    }
+  }
